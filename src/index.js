5058c85deef95690b71d03664127d19a2b07a1ca
Superdegot <65909405+Superdegot@users.noreply.github.com>
2022-08-12 14:27:13 +0200

    Copied JS and CSS files, copied image

---
+++ src/index.js
@@ -0,0 +1,78 @@
+let now = new Date();
+console.log(now);
+
+let p = document.querySelector("p");
+
+let date = now.getDate();
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let year = now.getFullYear();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[now.getDay()];
+
+let months = [
+  "Junuary",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+let month = months[now.getMonth()];
+
+p.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}, ${year}`;
+
+//homework5
+function showWeather(response) {
+  console.log(response.data.name);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(city) {
+  let apiKey = "a7f68d2ec2f69660b714e7d26727f615";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#form").value;
+  search(city);
+}
+
+let searchForm = document.querySelector("form");
+searchForm.addEventListener("submit", handleSubmit);
+
+search("Budva");
